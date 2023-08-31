ee1cd4279af1b139dabd5fcfbb3264e6e21e0e93
clambin <christophe.lambin@gmail.com>
2023-08-31 22:45:15 +0200

    first version

--- cmd/fluxcd-exporter/fluxcd-exporter.go
+++ cmd/fluxcd-exporter/fluxcd-exporter.go
@@ -4,15 +4,14 @@ import (
 	"errors"
 	"flag"
 	"fluxcd-exporter/internal/collector"
-	"fmt"
+	"github.com/go-logr/logr"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"log/slog"
 	"net/http"
 	"os"
-	"path/filepath"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var debug = flag.Bool("debug", false, "switch on debug logging")
@@ -26,36 +25,18 @@ func main() {
 	}
 	logger := slog.New(slog.NewTextHandler(os.Stderr, &opts))
 
-	cfg, err := getK8sConfig()
-	if err != nil {
-		panic(err)
-	}
+	l := logr.New(log.NullLogSink{})
+	log.SetLogger(l)
 
 	c := collector.Collector{
-		Config: cfg,
+		Config: config.GetConfigOrDie(),
 		Logger: logger,
 	}
 	prometheus.MustRegister(c)
 
 	http.Handle("/metrics", promhttp.Handler())
 
-	if err = http.ListenAndServe(":8080", nil); !errors.Is(err, http.ErrServerClosed) {
+	if err := http.ListenAndServe(":8080", nil); !errors.Is(err, http.ErrServerClosed) {
 		panic(err)
 	}
-}
-func getK8sConfig() (*rest.Config, error) {
-	if cfg, err := rest.InClusterConfig(); err == nil {
-		return cfg, nil
-	}
-
-	// not running inside cluster. try to connect as external client
-	userHomeDir, err := os.UserHomeDir()
-	if err != nil {
-		return nil, fmt.Errorf("user home dir: %w", err)
-	}
-	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
-	slog.Debug("not running inside cluster. using kube config", "filename", kubeConfigPath)
-
-	cfg, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	return cfg, err
 }
