e56d53f05cde3d8dfb19823b52eeb657df513fa7
Christophe Lambin <christophe.lambin@gmail.com>
2024-02-06 12:46:29 +0100

    test: code coverage

--- internal/flux/lister_test.go
+++ internal/flux/lister_test.go
@@ -1,9 +1,15 @@
 package flux
 
 import (
+	fluxHelmV2Beta2 "github.com/fluxcd/helm-controller/api/v2beta2"
+	fluxKustomizeV1 "github.com/fluxcd/kustomize-controller/api/v1"
+	fluxSourceV1 "github.com/fluxcd/source-controller/api/v1"
+	fluxSourceV1Beta2 "github.com/fluxcd/source-controller/api/v1beta2"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
+	"testing"
 )
 
 type testCase struct {
@@ -12,4 +18,18 @@ type testCase struct {
 	objects       []runtime.Object
 	wantErr       assert.ErrorAssertionFunc
 	want          []Resource
+}
+
+func Test_makeClient(t *testing.T) {
+	for _, builder := range []SchemeBuilder{
+		fluxSourceV1Beta2.SchemeBuilder,
+		fluxSourceV1.SchemeBuilder,
+		fluxHelmV2Beta2.SchemeBuilder,
+		fluxKustomizeV1.SchemeBuilder,
+	} {
+		assert.NotNil(t, makeClient(&rest.Config{}, builder))
+		assert.Panics(t, func() {
+			_ = makeClient(nil, builder)
+		})
+	}
 }
